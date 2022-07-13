57c24b106fce34cd14a625a325af4240f8912a46
Aurora <2200899041@qq.com>
2022-07-13 18:13:52 +0800

    数据类型

---
+++ 2_数据类型.C
@@ -0,0 +1,16 @@
+#include <stdio.h> 
+int main(){
+    char ch = 'a';
+    //整型
+    int age=20;
+    //短整型
+    short num=10;
+    //长整型
+    long long;
+
+    //单精度浮点型
+    float weight=66.6;
+    //双精度浮点型
+    double d=0.0;
+    return 0;
+}  
