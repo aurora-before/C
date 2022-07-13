27e70e88b1472ccde4753013651f9ee8705024f4
Aurora <2200899041@qq.com>
2022-07-13 12:51:21 +0800

    printf

---
+++ printf.C
@@ -0,0 +1,12 @@
+// 1.写出主函数main
+// 2.从主函数第一行开始执行（main入口函数）
+// 3.printf-库函数-打印，需引用头文件studio.h
+#include <stdio.h> 
+//int是返回的类型
+// 一个工程可以有多个.c文件
+// 多个.c文件中只能有一个main函数
+int main()
+{
+    printf("hello");
+    return 0;
+}
