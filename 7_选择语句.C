47f4609a93a49a084007508e02331081385cfd36
Aurora <2200899041@qq.com>
2022-08-04 16:06:39 +0800

    选择语句

---
+++ 7_选择语句.C
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int input = 0;
+    printf("请输入(1,0)");
+    scanf("%d", &input);
+    if (input == 1)
+        printf("好\n");
+    else
+        printf("卖红薯\n");
+    return 0;
+}
