49e9adbafa9a33b52162c030d76dc420d5d6deac
Aurora <2200899041@qq.com>
2022-08-04 18:01:04 +0800

    循环语句

--- 7_选择语句.C
+++ 7_选择语句.C
@@ -5,8 +5,12 @@ int main()
     printf("请输入(1,0)");
     scanf("%d", &input);
     if (input == 1)
+    {
         printf("好\n");
+    }
     else
+    {
         printf("卖红薯\n");
+    }
     return 0;
 }
