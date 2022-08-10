a5a02c76b121fd735e11eae8743f66d51c12b03e
Aurora <2200899041@qq.com>
2022-08-10 22:19:49 +0800

    关键字

--- 11_关键字.c
+++ 11_关键字.c
@@ -15,20 +15,24 @@
 
 // }
 #include <stdio.h>
+static int g_val=2022;
 void test(){
-    int a=1;
+    static int a=1;
     a++;
     printf("%d",a);
 }
 int main(){
     //        //static静态的
+    // static修饰局部变量，改变了局部变量的生命周期
 //        //1.static修饰局部变量
-//        //1.static修饰全局变量
+//        //1.static修饰全局变量,这个全局变量只能在自己所在源文件(.c)内部可以使用
 //        //1.static修饰函数
-    int i=0;
-    while (i<10){
-        test();
-        i++;
-    }
+    // int i=0;
+    // while (i<10){
+    //     test();
+    //     i++;
+    // }
+
+    printf("%d",g_val);
     return 0;
 }
