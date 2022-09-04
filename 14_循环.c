7b627ee713a3bff5004cf61ca55980c382378602
Aurora <2200899041@qq.com>
2022-09-04 15:48:03 +0800

    循环

--- 14_循环.c
+++ 14_循环.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+#include <string.h>
+#include <windows.h>
+#include <stdlib.h>
+#include <time.h>
 /*
 int main(){
     int ch=getchar();
@@ -106,3 +110,122 @@ int main()
     return 0;
 }
 */
+/*
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int k = 7;
+    int sz = sizeof(arr)/sizeof(arr[0]);
+    int left = 0;
+    int right = sz - 1;
+    while (left <= right)
+    {
+        int mid = (left + right) / 2;
+        if (arr[mid] < k)
+        {
+            left = mid + 1;
+        }
+        else if (arr[mid] > k)
+        {
+            right = mid - 1;
+        }
+        else
+        {
+            printf("找到了:%d\n", mid);
+            break;
+        }
+    }
+    if (left > right)
+    {
+        printf("找不到\n");
+    }
+    return 0;
+}
+*/
+
+/*
+//登录
+int main()
+{
+    int n=0;
+    char pwd[20] = {0};
+    char password[20]={"222"};
+    while (n <3)
+    {
+        printf("请输入密码");
+        scanf("%s",&pwd);
+        printf("%s\n",pwd);
+        printf("%s",password);
+        if(strcmp(pwd,password)==0){
+            printf("登录成功");
+            break;
+        }else{
+            printf("密码错误");
+        }
+        n++;
+        if(n==3){
+            break;
+        }
+    }
+    return 0;
+}
+*/
+
+//猜数字
+void menu()
+{
+    printf("****************");
+}
+void game()
+{
+    //生成随机数
+    // rand函数返回一个0-32767之间的数字
+    //时间-时间戳
+    //!!!
+    int ret = rand() % 100 + 1; //模100的余数是0-99，然后+1，范围就是1-100
+    printf("%d", ret);
+    //猜数字
+    int guess = 0;
+
+    while (1)//死循环
+    {
+        printf("请猜数字>");
+        scanf("%d", &guess);
+        if (guess < ret)
+        {
+            printf("猜小了\n");
+        }
+        else if (guess > ret)
+        {
+            printf("猜大了\n");
+        }
+        else
+        {
+            printf("恭喜你，猜对了\n");
+            break;
+        }
+    }
+}
+int main()
+{
+    int input = 0;
+    srand((unsigned int)time(NULL));
+    do
+    {
+        menu();
+        printf("请选择:>");
+        scanf("%d", &input);
+        switch (input)
+        {
+        case 1:
+            game();
+            break;
+        case 0:
+            printf("退出游戏\n");
+            break;
+        default:
+            printf("输入错误\n");
+            break;
+        }
+    } while (input);
+}
