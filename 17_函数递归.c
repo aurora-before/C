4b9a540c5bfd531ca45df783204b3ef04ff416f4
Aurora <2200899041@qq.com>
2022-09-25 15:38:29 +0800

    函数递归

--- 17_函数递归.c
+++ 17_函数递归.c
@@ -35,24 +35,28 @@ int main()
 }
 */
 
-
 /*
-int my_strlen(char* str){
-    int count=0;
-    if(*str!='\0'){
-        count++;
-        str++;
-        printf("%d %s\n",count,str);
-    }
-    return count;
-}
-*/
+模拟strlen
+//创建临时变量
+// int my_strlen(char* str){
+//     int count=0;
+//     if(*str!='\0'){
+//         count++;
+//         str++;
+//         printf("%d %s\n",count,str);
+//     }
+//     return count;
+// }
+
 //!!不允许创建临时变量，模拟strlen(使用递归实现)
 int my_strlen(char *str)
 {
     if (*str != '\0')
     {
         return 1 + my_strlen(str+1);
+        //不能使用后置++，因为后置++是先使用后+1
+        //可以前置++
+        // return 1 + my_strlen(str++);
     }
     else
     {
@@ -67,3 +71,64 @@ int main()
     printf("%d\n", my_strlen(arr));
     return 0;
 }
+*/
+
+/*
+//求n的阶乘
+int Fac(int n){
+    if(n>1){
+        return n*Fac(n-1);
+    }else{
+        return 1;
+    }
+}
+
+int main(){
+    int n=0;
+    scanf("%d",&n);
+    int ret=Fac(n);
+    printf("%d\n",ret);
+    return 0;
+}
+*/
+
+
+/*
+//求第n个斐波那契数
+//用递归效率太低
+//  1 1 2 3 5 8 13...
+// int Fib(int n)
+// {
+//     if (n <= 2)
+//     {
+//         return 1;
+//     }
+//     else
+//     {
+//         return Fib(n - 1) + Fib(n - 2);
+//     }
+// }
+int Fib(int n){
+    int a=1;
+    int b=1;
+    int c=1;
+    //迭代
+    while (n>2)
+    {
+        c=a+b;
+        a=b;
+        b=c;
+        n--;
+    }
+    return c;
+}
+int main()
+{
+    int n = 0;
+    scanf("%d", &n);
+    int ret = Fib(n);
+    printf("%d\n", ret);
+    return 0;
+}
+
+*/
